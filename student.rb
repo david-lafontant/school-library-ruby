38da4133cac1ffefa22910d158570c7d0bffbd73
Sumair <sumairqaisar992@gmail.com>
2022-04-20 03:13:26 +0500

    Add Student class

---
+++ student.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative 'person'
+# Student class inherits from Person
+class Student < Person
+  def initialize(*args, classroom)
+    super
+    @classroom = classroom
+  end
+
+  def play_hookey
+    p '¯\\(ツ)/¯', @classroom, @id
+  end
+end
