4f4c41add4b6618b4a4f3b4fd043487693d0f0e2
Sumair <sumairqaisar992@gmail.com>
2022-04-21 06:22:42 +0500

    Implement decorator pattern

--- person.rb
+++ person.rb
@@ -1,6 +1,9 @@
+require_relative 'nameable'
+
 # Parent class
-class Person
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -12,6 +15,10 @@ class Person
   attr_accessor :name, :age
 
   attr_reader :id
+
+  def correct_name
+    @name
+  end
 
   # setter methods
 
@@ -39,3 +46,45 @@ class Person
     end
   end
 end
+
+class Decorator < Nameable
+  attr_accessor :nameable
+
+  # @param [Component] component
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  # The Decorator delegates all work to the wrapped component.
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    if @nameable.correct_name.length > 10
+      str = []
+      10.times do |i|
+        str.push(@nameable.correct_name.chars[i])
+      end
+      str.join
+    else
+      @nameable.correct_name
+    end
+  end
+end
+
+person = Person.new(22, 'maximilianus')
+person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+capitalized_trimmed_person.correct_name
