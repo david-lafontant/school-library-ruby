28909cae65dddc28c3ab0ef39db30514318d7030
Sumair <sumairqaisar992@gmail.com>
2022-04-22 23:53:13 +0500

    Fix linter errors and fix rubocop.yml

--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,7 @@ require_relative 'person'
 # Teacher class inherits from Person
 class Teacher < Person
   attr_reader :specialization, :id
+
   def initialize(*_args, specialization)
     super
     @specialization = specialization
