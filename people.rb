d2063786da09734f4d74f74f4d927c9413775773
david-lafontant <lafontant.david@gmail.com>
2022-04-25 14:32:59 -0600

    Add create_student method

--- people.rb
+++ people.rb
@@ -18,5 +18,27 @@ class People
         puts 'Teacher created succesfully'
         reset
       end
-    
+      
+      def create_student
+        age = 0
+        permision = true
+        input = ''
+        until age.positive?
+          puts 'age:'
+          age = gets.chomp.to_i
+        end
+        puts 'name:'
+        name = gets.chomp
+        if age < 18
+          until %w[Y N].include?(input)
+            puts 'has parents permission?[Y,N]'
+            input = gets.chomp.upcase
+            permision = input == 'Y'
+          end
+        end
+        student = Student.new(nil, age, name, parent_permission: permision)
+        @people.push(student)
+        puts 'Student created succesfuly!'
+        reset
+      end
 end
