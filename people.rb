2c1c2790c4444338735908dd5af7b204872c61ce
david-lafontant <lafontant.david@gmail.com>
2022-04-25 14:31:43 -0600

    Add create_teacher method

--- people.rb
+++ people.rb
@@ -1,2 +1,22 @@
 class People
+
+
+
+    def create_teacher
+        age = 0
+        until age.positive?
+          puts 'age: '
+          age = gets.chomp.to_i
+        end
+        puts 'name:'
+        name = gets.chomp
+        puts 'Specialization:'
+        specialization = gets.chomp
+        teacher = Teacher.new(specialization, age, name)
+        @people.push(teacher)
+        puts teacher.name, teacher.age, teacher.specialization
+        puts 'Teacher created succesfully'
+        reset
+      end
+    
 end
