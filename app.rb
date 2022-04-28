1ec5d6da17d0ca4591ed14678970b6828c26492b
Sumair <sumairqaisar992@gmail.com>
2022-04-28 21:15:46 +0500

    Implement the storage and retrieval of data in the main app

--- app.rb
+++ app.rb
@@ -3,14 +3,17 @@ require_relative './books'
 require_relative 'people'
 require_relative 'list_printer'
 require_relative './rental'
-require_relative './save_data'
-require_relative './read_data'
+require_relative 'storage'
 
 class App
   def initialize
+    @storage = Storage.new
     @books = Books.new
     @people = People.new
     @rentals = []
+    @storage.read(@books, 'books.json')
+    @storage.read(@people, 'people.json')
+    @storage.rentals_read(@rentals, @people, @books, 'rentals.json')
     @num = 0
   end
 
@@ -27,13 +30,13 @@ class App
     @num = gets.chomp.to_i
 
     case @num.to_i
-    when 1 then ListPrinter.print_list(read_books_data)
+    when 1 then ListPrinter.print_list(@books.list)
     when 2 then ListPrinter.print_list(@people.list)
     when 3 then add_person
     when 4 then @books.add_book
     when 5 then create_rental
     when 6 then list_rentals
-    when 7 then puts 'exiting'
+    when 7 then handle_exit
     end
   end
 
@@ -51,15 +54,15 @@ class App
   def create_rental
     book_index = -1
     person_index = -1
-    list_of_book = read_books_data
+    # list_of_book = read_books_data
     puts 'select a book from the following list by number'
-    ListPrinter.print_list(list_of_book)
-    until book_index > -1 && book_index < list_of_book.length
+    ListPrinter.print_list(@books.list)
+    until book_index > -1 && book_index < @books.list.length
       puts 'book index:'
       book_index = gets.chomp.to_i
     end
     puts 'select a person from the following list by number'
-    list_of_people = 
+    # list_of_people = 
     ListPrinter.print_list(@people.list)
     until person_index > -1 && person_index < @people.list.length
       puts 'person index:'
@@ -71,11 +74,9 @@ class App
     #Before
     #Rental.new(date, @people.filter_with_index(person_index), @books.filter_with_index(book_index))
     #After
-    @rentals.push(Rental.new(date, @people.filter_with_index(person_index), @books.filter_with_index(book_index)))
-    puts @people.list
-    for rental in @rentals do
-      puts rental.person.class
-    end
+    @rentals << Rental.new({ 'date' => date, 'person' => @people.filter_with_index(person_index),
+      'book' => @books.filter_with_index(book_index) })
+   
     #  save_rentals_data(@rentals)
     #End 
     puts 'Rental created succesfully'
@@ -93,13 +94,21 @@ class App
     id = 0
     until id.positive?
       puts 'id of person: '
+      ListPrinter.print_list(@people.list)
       id = gets.chomp.to_i
     end
     puts 'rentals: '
-    ListPrinter.print_list(@people.list)
+    # ListPrinter.print_list(@people.list)
     @people.filter_with_id(id).rentals.each do |rental|
       puts "Date: #{rental.date}, book: #{rental.book.title} by #{rental.book.author}"
     end
     reset
+  end
+
+  def handle_exit
+    @storage.write(@books.list, 'books.json')
+    @storage.write(@people.list, 'people.json')
+    @storage.write(@rentals, 'rentals.json')
+
   end
 end
