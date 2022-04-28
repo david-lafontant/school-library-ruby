26bbaf0e6a4f610edb3012adfe81d7de75e9bfb2
Sumair <sumairqaisar992@gmail.com>
2022-04-28 21:14:30 +0500

    Alter student and teacher class

--- teacher.rb
+++ teacher.rb
@@ -1,15 +1,19 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_reader :specialization
+  attr_reader :specialization, :id
 
-  def initialize(specialization, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
-    @specialization = specialization
+  def initialize(args)
+    super(args['age'], args['id'], args['name'])
+    @specialization = args['specialization'] || nil
   end
 
   def to_s
     "[Teacher] #{super} specialization: #{specialization}"
+  end
+
+  def to_object
+    { class_name: 'Teacher', opt: { specialization: @specialization, id: @id, name: @name, age: @age } }
   end
 
   def can_use_services?
