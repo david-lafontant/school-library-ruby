cfbc466f4e37819256dc0cf81e4867156eb0c450
Sumair <sumairqaisar992@gmail.com>
2022-04-29 00:50:54 +0500

    Implement person test

---
+++ __test__/person_spec.rb
@@ -0,0 +1,34 @@
+require_relative 'helper'
+
+describe Person do
+  context 'when create a new person' do
+    it 'return an object who is an instance of Person class' do
+      person = Person.new(30, 17, name= 'Sam')
+      expect(person).to be_an_instance_of(Person)
+    end
+
+    it 'return the correct name' do
+        person = Person.new(30, 17, name= 'Sam')
+        expect(person.correct_name).to eql('Sam')
+      end
+
+    it 'return the correct name and age' do
+        person = Person.new(30, 17, name= 'Sam') 
+        expect(person.age).to eql(30)
+        expect(person.name).to eql('Sam')
+      end
+
+      it 'return true if the person can use services' do
+        person = Person.new(30, 17, name= 'Sam')
+        expect(person.can_use_services?).to be(true)
+      end
+  
+      it 'return true if age is greater or equal to 18' do
+        person = Person.new(30, 17, name= 'Sam')
+        expect(person.of_age?).to be(true)
+      end
+  
+
+
+  end
+end
